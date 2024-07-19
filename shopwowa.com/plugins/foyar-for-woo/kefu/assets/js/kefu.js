375d93ca072c054f3405ef98a4fce2db2c743897
hzdu <iado@foxmail.com>
2024-07-19 12:47:43 +0800

    update

---
+++ shopwowa.com/plugins/foyar-for-woo/kefu/assets/js/kefu.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("a.moquu_wmaps").on("click", function () {
+        popWin.showWin(
+            "400",
+            "800",
+            "Wowa Play",
+            "https://wowaplay.shopwowa.com"
+        );
+    });
+});
