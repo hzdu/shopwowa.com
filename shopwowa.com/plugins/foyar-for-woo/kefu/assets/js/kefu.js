c33b976de0d60dbeb056d719556ec5544d29ee22
hzdu <iado@foxmail.com>
2024-07-21 18:48:40 +0800

    update

--- shopwowa.com/plugins/foyar-for-woo/kefu/assets/js/kefu.js
+++ shopwowa.com/plugins/foyar-for-woo/kefu/assets/js/kefu.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function () {
     jQuery("a.moquu_wmaps").on("click", function () {
         popWin.showWin(
             "400",
-            "800",
+            "600",
             "Wowa Play",
             "https://wowaplay.shopwowa.com"
         );
